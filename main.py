f605e6b93e6daa25d6cd756e259e8d5aa493bc02
wpdrbischoff <rbischoff@wilpark.org>
2022-11-16 21:00:23 -0600

    working sasearchindex w/ offset

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ api = RectracAPI.API()
 
 api.auth(username=wpdconfig.USERNAME,password=wpdconfig.PASSWORD)
 
+#testing facility
 #params = {'fields': 'FacilityCode', 'limit': 55}
 #data = api.query_endpoint(endpoint='facilities', params=params)
 #PoC_print_data(data)
 
+#testing households
 #params = {'limit': 5}
 #data = api.query_endpoint(endpoint='households', params=params)
 #PoC_print_data(data)
 
+
+#testing table query  / / gets all active, display on web, sections
 params = {'tablename': 'SASearchIndex', 
         'limit': 999, 
         'filterField': 'WordIndex', 
@@ -23,4 +27,17 @@ table = 'SASearchIndex'
 
 data = api.query_endpoint(endpoint=f'tables/{table}/data', params=params)
 print(len(data['SASearchIndex']))
-print(data['nextpage'])
+#print(data['nextpage'])
+
+#testing offset table query
+params = {'tablename': 'SASearchIndex', 
+        'limit': 999,
+        'offset': 999,
+        'filterField': 'WordIndex', 
+        'FilterBy': 'contains',
+        'FilterValue': 'MO:AR*WB:Yes*MS:Active'}
+
+table = 'SASearchIndex'
+
+data = api.query_endpoint(endpoint=f'tables/{table}/data', params=params)
+print(len(data['SASearchIndex']))
