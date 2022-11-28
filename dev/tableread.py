224134d56515bb131078b02d729d64e3d063f63f
wpdrbischoff <rbischoff@wilpark.org>
2022-11-28 15:11:41 -0600

    fixed tableread

--- dev/tableread.py
+++ dev/tableread.py
@@ -8,7 +8,7 @@ api.auth(username=wpdconfig.USERNAME,password=wpdconfig.PASSWORD)
 
 #testing table query  / / gets all active, display on web, sections
 params = {'tablename': 'SASearchIndex',
-        'limit': 5,
+        'limit': 999,
         'filterField': 'WordIndex',
         'FilterBy': 'contains',
         'FilterValue': 'MO:AR*WB:Yes*MS:Active'}
@@ -21,11 +21,11 @@ pprint.pprint(data)
 
 #testing offset table query
 #params = {'tablename': 'SASearchIndex',
-        'limit': 2,
-        'offset': 999
-        'filterField': 'WordIndex',
-        'FilterBy': 'contains',
-        'FilterValue': 'MO:AR*WB:Yes*MS:Active'}
+#        'limit': 2,
+#        'offset': 999
+#        'filterField': 'WordIndex',
+#        'FilterBy': 'contains',
+#        'FilterValue': 'MO:AR*WB:Yes*MS:Active'}
 
 #data = api.query_endpoint(endpoint=f'tables/{table}/data', params=params)
 #pprint.pprint(data['SASearchIndex'])
